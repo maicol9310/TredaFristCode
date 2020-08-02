c8b1e047c257f85a54d3a269c20cda507e73026a
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2020-08-02 16:39:15 -0500

    Se crea un nuevo proyecto mvc para el front, se implementan las clases modelos para consumir las APIs

---
+++ TredaFront/TredaFront/Models/ForStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TredaFront.Models
+{
+    public class ForStore
+    {
+        [Display(Name = "SKU")]
+        public int SKU { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Value")]
+        public decimal Value { get; set; }
+
+        [Display(Name = "Base 64 Imagen")]
+        public string Base64Imagen { get; set; }
+    }
+}
---
+++ TredaFront/TredaFront/Models/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TredaFront.Models
+{
+    public class Products
+    {
+        [Display(Name = "SKU")]
+        public int SKU { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Value")]
+        public decimal Value { get; set; }
+
+        [Display(Name = "Store")]
+        public int Store { get; set; }
+
+        [Display(Name = "Base 64 Imagen")]
+        public string Base64Imagen { get; set; }
+    }
+}
